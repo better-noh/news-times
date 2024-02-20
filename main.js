0c8d9b89f527dd65b5913d3bc858fae8dde40453
better-noh <merrynaeun@gmail.com>
2024-02-21 01:45:20 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// apiKey 를 넣는 변수
+const API_KEY = `3b98b139fbdd4256b21074309b347b39`;
+// news 를 전역변수로 선언
+let news = [];
+
+// 뉴스를 가져오기 위한 함수
+const getLatestNews = async ()=> {
+    // url 주소를 넣는 변수 : URL 인스턴스 활용
+    const url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        );
+    // url 호출
+    const response = await fetch(url);
+    const data = await response.json();
+    news = data.articles;
+    console.log("news:",news);
+};
+
+getLatestNews();
---
+++ main2.js
@@ -0,0 +1,3 @@
+console.log(1);
+setTimeout(() => console.log(2), 5000);
+console.log(3);
