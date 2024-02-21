060d2e44bbae69227c627c14b39fc63aebb06a9c
better-noh <merrynaeun@gmail.com>
2024-02-22 01:58:31 +0900

    third commit

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 // apiKey 를 넣는 변수
 const API_KEY = `3b98b139fbdd4256b21074309b347b39`;
 // news 를 전역변수로 선언
-let news = [];
+let newsList = [];
 
 // 뉴스를 가져오기 위한 함수
 const getLatestNews = async ()=> {
     // url 주소를 넣는 변수 : URL 인스턴스 활용
+    // 제출용 url 
     const url = new URL(
         `https://resilient-lebkuchen-c3be7b.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
         );
+    // 실습용 url
+    // const url = new URL(
+    //     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    //     );
     // url 호출
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("news:",news);
+    newsList = data.articles;
+    render();
+    console.log("news:",newsList);
 };
 
+const render = () => {
+    // UI 내용
+    const newsHTML = newsList.map(news=>`<!-- 뉴스 -->
+    <div class="row news">
+        <!-- 이미지 -->
+        <div class="col-lg-4">
+            <img class="news-img-size" src=${news.urlToImage}/>
+        </div>
+        <!-- 텍스트 -->
+        <div class="col-lg-8">
+            <!-- 타이틀 -->
+            <h2>${news.title}</h2>
+            <!-- 내용 -->
+            <p>${news.description}</p>
+            <!-- 일반 설명 -->
+            <div>${news.source.name} * ${news.publishedAt}</div>
+        </div>
+    </div>`
+    ); 
+
+    // "news-board" 에 붙여서 newsHTML 을 보여준다.
+    document.getElementById("news-board").innerHTML = newsHTML;
+}
 getLatestNews();
--- main2.js
+++ main2.js
@@ -1,3 +1,17 @@
-console.log(1);
-setTimeout(() => console.log(2), 5000);
-console.log(3);
+const API_KEY = "3b98b139fbdd4256b21074309b347b39";
+
+function getNews() {
+    let url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    );
+    console.log("uuu",url);
+
+    const response = fetch(url);
+
+    console.log("rrr",response);
+}
+
+getNews();
+for (let i=0; i<20; i++) {
+    console.log("after",i);
+}
